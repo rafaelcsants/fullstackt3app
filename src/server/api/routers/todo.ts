c93ce5e9bd7c480cedf491229e072116a8d038cd
Rafael <rafaelcsants@hotmail.com>
2023-03-28 23:33:39 +0100

    type and todo router

---
+++ src/server/api/routers/todo.ts
@@ -0,0 +1,83 @@
+import { z } from "zod";
+import { todoInput } from "~/types";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+import { Input } from "postcss";
+
+export const todoRouter = createTRPCRouter({
+  //  GetAll ToDos
+  all: protectedProcedure.query(async ({ ctx }) => {
+    const todos = await ctx.prisma.todo.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+    console.log(
+      "todos from prisma",
+      todos.map(({ id, text, done }) => ({ id, text, done }))
+    );
+
+    return [
+      {
+        id: "fake",
+        text: "fake text",
+        done: false,
+      },
+      {
+        id: "fake2",
+        text: "fake text2",
+        done: true,
+      },
+    ];
+  }),
+
+  //  Criar um ToDo
+  create: protectedProcedure
+    .input(todoInput)
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.todo.create({
+        data: {
+          text: input,
+          user: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      });
+    }),
+
+  //  Apagar ToDo
+  delete: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.todo.delete({
+        where: {
+          id: input,
+        },
+      });
+    }),
+
+  //  TOGGLE/UPDATE ToDo (O SEU ESTADO DE 'DONE')
+  toggle: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        done: z.boolean(),
+      })
+    )
+    .mutation(async ({ ctx, input: { id, done } }) => {
+      return ctx.prisma.todo.update({
+        where: {
+          id,
+        },
+        data: {
+          done,
+        },
+      });
+    }),
+});
